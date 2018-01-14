ec2e97ef164e4c2deb1a227ce558aaa6cd5cf71b
J <flatironschool@2joyce.com>
2018-01-14 06:31:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ function removeFromCart(item) {
 //  for (var i = 0; i < cart.length; i++) {
     if (cart[i].hasOwnProperty(item) === true) {
       cart.splice(i,1);
-      break;
       return cart;
     }
   }
