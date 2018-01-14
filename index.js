3cad4e34a20c235ba9cbbcfc8f076c7dc322ba84
J <flatironschool@2joyce.com>
2018-01-14 06:06:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,9 @@ function removeFromCart(item) {
     if (cart[eachItem].hasOwnProperty(item)) {
       cart.splice(eachItem,1);
       return cart;
+    } else {
+      console.log('That item is not in your cart.');
+      return cart;
     }
   }
 }
